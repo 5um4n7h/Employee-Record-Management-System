0dd2bb2949c210178f60bb290124ead77d57ca3d
5um4n7h <sumanthhegde13@gmail.com>
2021-07-07 12:44:41 +0530

    menu added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+// Header Files
+#include <iostream> // For Basic I/O
+using namespace std;
+
+int mainmenu(); // fn that prints main menu on the output console
+
+
+bool validateInput();
+
+// class used in pgrm
+class Employee
+{
+
+};
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   mainmenu();
     return 0;
+}
+
+int mainmenu(){
+
+    int choice;
+    bool flag;
+    cout << "\n\n <================= Employee Managment System =================>\n\n";
+    cout << "\tPlease Select Your Choice :- \n";
+    cout << "\t1 : Insert Employees Records\n";
+    cout << "\t2 : View All Employees Records\n";
+    cout << "\t3 : Search Employees Records\n";
+    cout << "\t4 : Update Employee Records\n";
+    cout << "\t5 : Delete Employee Records\n";
+    cout << "\t6 : Exit Application\n";
+    label:
+    cout << "\tEnter Your Choice : ";
+    cin >> choice;
+
+    flag = validateInput();
+    if (flag)
+        goto label;
+    else
+        return choice;
+
+}
+
+bool validateInput() {
+    //TODO
+    return false;
 }
